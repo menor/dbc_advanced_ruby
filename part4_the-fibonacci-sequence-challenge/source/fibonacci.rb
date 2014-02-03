6b2740ab350ec239dd9e6cf44bcb27eaecd908d8
& <pair+harryw377.menor@devbootcamp.com>
2014-02-03 12:32:55 -0800

    added slow recursion and benchmarked

--- part4_the-fibonacci-sequence-challenge/source/fibonacci.rb
+++ part4_the-fibonacci-sequence-challenge/source/fibonacci.rb
@@ -28,16 +28,29 @@ def fibonacci_recursive(num,current_fib=1, prior_fib=0)
   fibonacci_recursive(num-1, current_fib+prior_fib, current_fib)
 end
 
-p fibonacci_iterative(0) == 0
-p fibonacci_iterative(1) == 1
-p fibonacci_iterative(2) == 1
-p fibonacci_iterative(3) == 2
-p fibonacci_iterative(4) == 3
-p fibonacci_iterative(5) == 5
+def fibonacci_slow(n)
+   n <= 1 ? n :  fibonacci_slow( n - 1 ) + fibonacci_slow( n - 2 )
+end
 
-p fibonacci_recursive(0) == 0
-p fibonacci_recursive(1) == 1
-p fibonacci_recursive(2) == 1
-p fibonacci_recursive(3) == 2
-p fibonacci_recursive(4) == 3
-p fibonacci_recursive(5) == 5
+# p fibonacci_iterative(0) == 0
+# p fibonacci_iterative(1) == 1
+# p fibonacci_iterative(2) == 1
+# p fibonacci_iterative(3) == 2
+# p fibonacci_iterative(4) == 3
+# p fibonacci_iterative(5) == 5
+
+# p fibonacci_recursive(0) == 0
+# p fibonacci_recursive(1) == 1
+# p fibonacci_recursive(2) == 1
+# p fibonacci_recursive(3) == 2
+# p fibonacci_recursive(4) == 3
+# p fibonacci_recursive(5) == 5
+
+require 'benchmark'
+
+n = 1000
+Benchmark.bmbm(7) do |x|
+  x.report("fibonacci_iterative")   { n.times { fibonacci_iterative(10) } }
+  x.report("fibonacci_recursive") { n.times { fibonacci_recursive(10) } }
+  x.report("fibonacci_slow") { n.times { fibonacci_slow(10) } }
+end
