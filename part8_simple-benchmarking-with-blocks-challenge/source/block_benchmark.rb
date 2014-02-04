bfcf6473d363f2ffc572f88baf3e645208715b4f
& <pair+harryw377.menor@devbootcamp.com>
2014-02-03 16:24:19 -0800

    Part8 finished

--- part8_simple-benchmarking-with-blocks-challenge/source/block_benchmark.rb
+++ part8_simple-benchmarking-with-blocks-challenge/source/block_benchmark.rb
@@ -1,5 +1,8 @@
 def benchmark
-  # Your benchmarking code goes here.
+  start_time = Time.now
+  yield
+  end_time = Time.now
+  end_time - start_time
 end
 
 # Be careful, pasting this into IRB will take a long time to print.
@@ -9,3 +12,4 @@ long_string = "apple"*100000000
 running_time = benchmark { long_string.reverse }
 
 puts "string.reverse took #{running_time} seconds to run"
+
